fda9dc740dfca6b1f5bab79415f91eb5eb896d6f
bibleaffliction11 <bible_affliction11@yahoo.com>
2016-08-24 13:52:11 +0800

    Add files via upload
    Array Stack for DSA

---
+++ ysra.java
@@ -0,0 +1,95 @@
+
+
+package ysra;
+import java.util.*;
+public class ysra {
+    
+
+  private int capacity=5;
+  private int top = 1;
+  private String[] storage;
+  
+  public ysra(int size) {
+    capacity =size;
+    storage = new String[capacity];
+  } 
+
+  public void show() {
+    for (int Y = capacity-1; Y >= 0; Y--) { 
+      for (int S = capacity-1; S >= 0; S--) {
+          System.out.println("stack["+Y--+"]="+storage[S]);
+      }
+    }
+    System.out.println();
+  }
+  
+  private boolean isEmpty() {
+    if (storage[top] == null) {
+      System.out.println("EMPTY! :(");
+      return true;
+    } 
+    return false;
+  }
+
+  private boolean isFull() {
+    if (top == capacity) {
+      System.out.println("FULL! :(");
+      return true;
+    } 
+    return false;
+  }
+
+  public void push(String value) {
+    if (isFull()) {
+      
+      System.out.println("FAILED");
+    } else {
+      System.out.println("... Push[" + (top) + "] ...");
+      storage[top] = value;
+      top++;
+      System.out.println(value + " was successfully added.");
+      System.out.println();
+    }
+  }
+
+  public void pop() {
+    if (isEmpty()) {
+      System.out.println();
+    } else {
+      System.out.println("... trying to pop stack[" + (top-1) + "] ...");
+      storage[top] = storage[10];
+      System.out.println(storage[top] + " was successfully removed.");
+      System.out.println();
+    }
+  }
+
+  public void peek() {
+    if (storage[top] == storage[0]) {
+      System.out.println("PEEK TOP = " + storage[top]);
+      System.out.println();
+    } else {
+      System.out.println("PEEK TOP = " + storage[top-1]);
+      System.out.println();
+    }
+  }
+  
+  public static void main(String[] args) {
+    ysra storage = new ysra(10);
+    System.out.println("STORAGE CAPACITY = " +storage.capacity);
+    System.out.println();
+    storage.peek();
+    storage.pop();
+    storage.push("one");
+    storage.push("two");
+    storage.push("three");
+    storage.push("four");
+    storage.push("five"); 
+    storage.push("six"); 
+    storage.push("seven"); 
+    storage.push("eight"); 
+    storage.push("nine");    
+    storage.push("ten"); 
+    
+  }  
+
+}
